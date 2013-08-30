5a3b5566c9d48b9237f777d4dabd1a445b6cbd82
gaurav <gaurav@ggvaidya.com>
2013-08-29 22:21:00 -0600

    Rewrote agrew.pl to use another GBIF API (see http://dev.gbif.org/issues/browse/PF-183 for Markus' suggestion).

--- reconciliation-wrapper/agrew.pl
+++ reconciliation-wrapper/agrew.pl
@@ -12,6 +12,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use URI::Escape;
 use Time::HiRes qw/time/;
 
 # Version and settings.
@@ -110,127 +111,22 @@ sub process_query {
     my $query_ref = shift;
     my %query = %$query_ref;
 
-    my @results;
-
     # Right now, we only use 'query'. Look up https://github.com/OpenRefine/OpenRefine/wiki/Reconciliation-Service-API#single-query-mode for other options.
     # Ideas:
     #   - use 'Family' for high-level filtering.
     my $name = $query{'query'};
-    my $name_in_url = $name;
-
     say STDERR "Query: '$name'";
 
-    # TODO: Better URLification.
-    $name_in_url =~ s/ /%20/g;
-
-    # In case of errors, try 10 times.
     my $request_time_start = time;
+    my @results = get_gbif_name_usages_for_name($name);
+    my $time_taken = (time - $request_time_start);
+    printf STDERR "  Retrieved %d matches for '$name' in %.4f ms.\n", (scalar @results), $time_taken*1000;
 
-    my $response;
-    for(my $x = 0; $x < 10; $x++) {
-        my $url = 'http://api.gbif.org/name_usage/' . $name_in_url;
-        $response = $ua->get($url);
+    my @summarized = summarize_name_usages(@results);
+    $time_taken = (time - $request_time_start);
+    printf STDERR "  Summarized '$name' to %d matches in %.4f ms.\n", (scalar @summarized), $time_taken*1000;
 
-        if($response->is_success) {
-            last;
-        } else {
-            warn "Could not connect to '$url': " . $response->status_line . "; retrying.";
-        }
-
-        sleep(2);
-    }
-
-    if($response->is_success) {
-        my $content = $response->decoded_content;
-        my $gbif_match = from_json($content);
-
-        if(0 == scalar @$gbif_match) {
-            # No matches.
-        } else {
-            # TODO: At this point, summarize down to one match per name. But we'll do that latter.
-            my %unique_matches;
-
-            my $time_taken = (time - $request_time_start);
-            printf STDERR "  %d matches found on GBIF for '$name' in %.4f ms.\n", (scalar @$gbif_match), ($time_taken*1000);
-            
-            foreach my $match (@$gbif_match) {
-                my $name = $match->{'canonicalName'} // $match->{'scientificName'};
-                my $accepted_name = $match->{'acceptedNameUsage'} // $match->{'accepted'} // ''; 
-                my $authority = $match->{'authorship'} // 'unknown';
-                my $kingdom = $match->{'kingdom'} // 'Life';
-
-                $unique_matches{$name}{$accepted_name}{$authority}{$kingdom} = []
-                    unless exists $unique_matches{$name}{$accepted_name}{$authority}{$kingdom};
-
-                push @{$unique_matches{$name}{$accepted_name}{$authority}{$kingdom}}, $match;
-            }
-
-            foreach my $name (sort keys %unique_matches) {
-                foreach my $accepted_name (sort keys %{$unique_matches{$name}}) {
-                    foreach my $authority (sort keys %{$unique_matches{$name}{$accepted_name}}) {
-                        foreach my $kingdom (sort keys %{$unique_matches{$name}{$accepted_name}{$authority}}) {
-                            my @matches = @{$unique_matches{$name}{$accepted_name}{$authority}{$kingdom}};
-
-                            # How do we summarize matches? EASY.
-                            my $gbif_key;
-
-                            my %summary;
-                            foreach my $match (@matches) {
-                                foreach my $field (keys %$match) {
-                                    my $value = $match->{$field};
-
-                                    $value = Dumper($value)
-                                        unless ref($value) eq '';
-
-                                    $summary{$field}{$value} = 0
-                                        unless exists $summary{$field}{$value};
-                                    $summary{$field}{$value}++;
-
-                                    unless(defined $gbif_key) {
-                                        if($field eq 'key') {
-                                            $gbif_key = $value;
-                                        }
-                                    }
-                                }
-                            }
-                            
-                            # Further simplify fields common for ALL checklists.
-                            my $match_count = scalar @matches;
-                            foreach my $field (keys %summary) {
-                                foreach my $value (keys %{$summary{$field}}) {
-                                    my $count = $summary{$field}{$value};
-
-                                    if($count == $match_count) {
-                                        $summary{$field} = $value;
-                                    }
-                                }
-                            }
-
-                            my %result;
-
-                            $result{'id'} = $gbif_key;
-                            $result{'name'} = "$name $authority";
-                            $result{'name'} .= " [=> $accepted_name]" unless $accepted_name eq '';
-                            $result{'name'} .= " ($kingdom)";
-                            $result{'type'} = ['http://uat.gbif.org/species/'];
-                            $result{'score'} = scalar @matches;
-                            $result{'match'} = $JSON::false;
-                            $result{'summary'} = \%summary;
-                            # $result{'full_gbif'} = $content;
-
-                            push @results, \%result;
-                        }
-                    }
-                }
-            }
-
-            $time_taken = (time - $request_time_start);
-            printf STDERR "  Summarized '$name' to %d matches in %.4f ms.\n", (scalar @results), $time_taken*1000;
-            
-        }
-    }
-
-    my @sorted_results = sort { $b->{'score'} <=> $a->{'score'} } @results;
+    my @sorted_results = sort { $b->{'score'} <=> $a->{'score'} } @summarized;
 
     # Add a dummy result so we know that all results are getting through.
     if($FLAG_DISPLAY_ENTRIES_END_HERE) {
@@ -243,7 +139,157 @@ sub process_query {
         };
     }
 
+    # TODO: rewrite this so we can send back error messages (even if OpenRefine can't do anything sensible with them).
+
     return { 'result' => \@sorted_results };
+}
+
+our $LIMIT_URL_RETRIES = 10;    # How often should this code try a URL (GET) request?
+our $SLEEP_BETWEEN_URL_RETRIES = 2; # How long should this code sleep between URL retries?
+
+sub retry_url_until_success($) {
+    my $url = shift;
+    my $response;
+
+    for(my $x = 0; $x < $LIMIT_URL_RETRIES; $x++) {
+        $response = $ua->get($url);
+
+        if($response->is_success) {
+            return $response;
+        } else {
+            warn "Could not connect to '$url': " . $response->status_line . "; retrying.";
+        }
+    
+        sleep($SLEEP_BETWEEN_URL_RETRIES);
+    }
+
+    warn "Giving up connecting to URL '$url': " . Dumper($response) . " (end of dumper output).";
+    return $response;
+}
+
+sub get_gbif_name_usages_for_name {
+    my $name = shift;
+    my $name_in_url = uri_escape($name);    # URLification.
+
+    my $response = retry_url_until_success("http://api.gbif.org/lookup/name_usage?strict=true&name=$name_in_url");
+    return unless ($response->is_success);
+        
+    my $content = $response->decoded_content;
+
+    # Four options here: we might have an invalid response, or a valid response which has:
+    #   'matchType' of 'NONE': no matches
+    #   'matchType' of 'FUZZY': under the current implementation, let's ignore this.
+    #   'matchType' of 'EXACT': perfect.
+    my $gbif_nub_match = from_json($content);   # This will croak on error, i.e. if given invalid input.
+
+    given($gbif_nub_match->{'matchType'}) {
+        when('NONE') {
+            # No matches.
+            return;
+        }
+
+        when('FUZZY') {
+            # Ignore fuzzy matches for now. We'll deal with them eventually.
+            return;
+        }
+    }
+
+    my $nub_key = $gbif_nub_match->{'usageKey'};
+
+    unless($nub_key =~ /^\d+$/) {
+        warn "ERROR: Invalid usageKey: got $nub_key, expected numeric; skipping request";
+        return;
+    }
+
+    # Find all related name usages to the taxon.
+    $response = retry_url_until_success("http://api.gbif.org/name_usage/$nub_key/related");
+    return unless ($response->is_success);
+
+    $content = $response->decoded_content;
+    my $gbif_related = from_json($content);
+
+    return @$gbif_related;
+}
+
+sub summarize_name_usages {
+    my @name_usages = @_;
+
+    # Summarize results.
+    my %unique_matches;
+    my @summarized;
+
+    foreach my $match (@name_usages) {
+        my $name = $match->{'canonicalName'} // $match->{'scientificName'};
+        my $accepted_name = $match->{'acceptedNameUsage'} // $match->{'accepted'} // ''; 
+        my $authority = $match->{'authorship'} // 'unknown';
+        my $kingdom = $match->{'kingdom'} // 'Life';
+
+        $unique_matches{$name}{$accepted_name}{$authority}{$kingdom} = []
+            unless exists $unique_matches{$name}{$accepted_name}{$authority}{$kingdom};
+
+        push @{$unique_matches{$name}{$accepted_name}{$authority}{$kingdom}}, $match;
+    }
+
+    foreach my $name (sort keys %unique_matches) {
+        foreach my $accepted_name (sort keys %{$unique_matches{$name}}) {
+            foreach my $authority (sort keys %{$unique_matches{$name}{$accepted_name}}) {
+                foreach my $kingdom (sort keys %{$unique_matches{$name}{$accepted_name}{$authority}}) {
+                    my @matches = @{$unique_matches{$name}{$accepted_name}{$authority}{$kingdom}};
+
+                    # How do we summarize matches? EASY.
+                    my $gbif_key;
+
+                    my %summary;
+                    foreach my $match (@matches) {
+                        foreach my $field (keys %$match) {
+                            my $value = $match->{$field};
+
+                            $value = Dumper($value)
+                                unless ref($value) eq '';
+
+                            $summary{$field}{$value} = 0
+                                unless exists $summary{$field}{$value};
+                            $summary{$field}{$value}++;
+
+                            unless(defined $gbif_key) {
+                                if($field eq 'key') {
+                                    $gbif_key = $value;
+                                }
+                            }
+                        }
+                    }
+                    
+                    # Further simplify fields common for ALL checklists.
+                    my $match_count = scalar @matches;
+                    foreach my $field (keys %summary) {
+                        foreach my $value (keys %{$summary{$field}}) {
+                            my $count = $summary{$field}{$value};
+
+                            if($count == $match_count) {
+                                $summary{$field} = $value;
+                            }
+                        }
+                    }
+
+                    my %result;
+
+                    $result{'id'} = $gbif_key;
+                    $result{'name'} = "$name $authority";
+                    $result{'name'} .= " [=> $accepted_name]" unless $accepted_name eq '';
+                    $result{'name'} .= " ($kingdom)";
+                    $result{'type'} = ['http://uat.gbif.org/species/'];
+                    $result{'score'} = scalar @matches;
+                    $result{'match'} = $JSON::false;
+                    $result{'summary'} = \%summary;
+                    # $result{'full_gbif'} = $content;
+
+                    push @summarized, \%result;
+                }
+            }
+        }
+    }
+
+    return @summarized;
 }
 
 start;
