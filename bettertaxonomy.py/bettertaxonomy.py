bb00e0d890d0cdaa6e8ff82674e0d3286606a7bd
gaurav <gaurav@ggvaidya.com>
2014-05-28 00:45:11 -0600

    Started work on a separate sources list.

--- bettertaxonomy.py/bettertaxonomy.py
+++ bettertaxonomy.py/bettertaxonomy.py
@@ -7,29 +7,64 @@ import csv
 import sys
 
 # Read the command line.
-cmdline = argparse.ArgumentParser(description = 'Match species names')
+cmdline = argparse.ArgumentParser(description = 'Matches species names against ' +
+    'several online databases and an optional, internal database.')
+
+# - Input files. Defaults to <stdin>.
 cmdline.add_argument('input', 
     nargs='*', 
     type=argparse.FileType('r'),
     help='A CSV or plain text file containing species names',
     default = [sys.stdin])
+
+# - fieldname: if species names are in the input file, this can be used to
+#   find them in any field. Case-sensitive.
 cmdline.add_argument('-fieldname',
     type=str,
     help='The field containing scientific names to match',
     default = 'scientificName')
+
+# - internal: the filename of a CSV file to use in an internal database. This
+#   is used to find species names, 
 cmdline.add_argument('-internal',
-    nargs='?',
     type=str,
     help='Internal list of name corrections (must be a CSV file)')
 
+# - sources: a file containing a list of databases to query.
+cmdline.add_argument('-sources',
+    type=str,
+    help='Sources of taxonomic names (see sources.example.txt for an example)'
+)
+
+# Parse command line.
 args = cmdline.parse_args()
 
+# Load and parse the list of sources.
+
+
 # Load the entire internal list, if it exists.
+# We expect the following columns:
+#   - (req) scientificName: the name to match
+#   - (req) correctName: the correct name to use
+#   - (req) source: the source for this correction
+internal_required_columns = set(['scientificName', 'correctName', 'source'])
 internal_corrections = dict()
 ic_fieldnames = None
 if args.internal:
-    reader = csv.DictReader(open(args.internal, "r"), dialect=csv.excel)
+    ic_file = open(args.internal, "rt")
+    reader = csv.DictReader(ic_file, dialect=csv.excel)
     ic_fieldnames = reader.fieldnames
+
+    # Check for missing required columns.
+    if not internal_required_columns.issubset(set(ic_fieldnames)):
+        sys.stderr.write("Missing column(s) in internal file '{0:s}': {1:s}\n".
+            format(
+                args.internal,
+                ', '.join(internal_required_columns - set(ic_fieldnames))
+            )
+        )
+        exit(1)
+
     row_index = 0
     for row in reader:
         row_index+=1
@@ -97,8 +132,12 @@ for input in args.input:
 
         output.writerow(row)
 
+# Add any unmatched names back to the internal database.
 if args.internal and len(unmatched_names) > 0:
-    internal_file = open(args.internal, mode="a")
+    # TODO: for some reason, this uses DOS-style line endings. We could
+    # try to detect line endings when we read the field earlier and then set
+    # it up here, but I'm too annoyed to fix that now.
+    internal_file = open(args.internal, mode="at")
     writer = csv.DictWriter(internal_file, ic_fieldnames, dialect=csv.excel)
     
     dict_row = dict()
